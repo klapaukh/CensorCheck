6fa41e68335ae68e9b3d30dbfbd723493108002e
Roma Klapaukh <rklapaukh@hotmail.com>
2024-03-25 16:43:24 +1100

    Test unicode

--- censor-check/src/test/java/au/edu/unimelb/habic/censor_check/DocumentTest.java
+++ censor-check/src/test/java/au/edu/unimelb/habic/censor_check/DocumentTest.java
@@ -1,5 +1,8 @@
 package au.edu.unimelb.habic.censor_check;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -7,14 +10,20 @@ import org.junit.jupiter.api.Test;
  */
 public class DocumentTest 
 {
-    /**
-     * Test that overlaps in Documents don't throw exceptions
-     */
 	@Test
-    public void testOverlap()
+    public void testBounds()
     {
 		Document rec = new Document("file.txt", "Hello world");
-		rec.addTrueMask(new Annotation("T1\tPHONE 4 7\to w"));
-        rec.addTrueMask(new Annotation("T1\tHOUSE 6 9\twor"));
+		rec.addTrueMask(new Annotation("T1\tPHONE 0 2\tHe"));
+        rec.addTrueMask(new Annotation("T1\tPHONE 9 11\tld"));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> rec.addTrueMask(new Annotation("T1\tPHONE 10 12\tld")));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> rec.addTrueMask(new Annotation("T1\tPHONE -1 2\tld")));
     }
+	
+	@Test
+	public void testLength()
+	{
+		assertEquals(new Document("file.txt", "H̷̛̙̫̹̹̻͕͇͓̼̙͔̪͓͖̟̦̯̘̫͖̻͔̞̞̩̑̑͐̒͒̅ͅͅe̶̯͇̥̳͓̼͉̩͙̾̀̇̉͊̓̊͆̌͗͊̂̄̌̀̒͆̋͋̑̓̿͋̐͘͠͝l̷̦̄͛̓͌̈̀̅̆͑́̋̉̎̚͠͝l̴̢͕̗̘͚͚͇̮̙̪̘̖͙̠̯̭̦̂̐̀̈́͂̽̎̄͑́̈́̕̕̚ͅͅo̸̢͉̤͓̝̱̝͖̰̰̘̜͊ ̵̛̘̥̝̙̫͈̪͊̈́͒̏̓̊͑͌̃̒̈̿̀̍͂̆͋̈͗͒͒͑̆̚͝͝W̷̧̢͙̻̺̤͕̝̝̠͇̫̺͖̳̹͙̞̬̟̓̀͑̌͊̿͑̅̎̀͗̍̈́̂̑̈́̽̅̈́̈́̈́̾̕͠͝o̸͖̒̋̀̊̅̓̌͆̆r̵̰͍̥̼͎͙̖̀͒̀̈́̀̊́̎͑̈́͝͝l̴͙̝̠̖̳͑͒̔̄̇̉̆̽́̃̌̉͋͂̓̂̓̇̾͋͛͘̕͝͠ḑ̵̡̨̭̠͓͖͎̲̹̦̙̱̗͓͕̮̺̲̼͚̜͙̖͍̭͍͗̓̊͗̈́̏͐̓̚͝").length(), 11);
+		assertEquals(new Document("file.txt", "a1👩‍🚀あ👨‍👩‍👧‍👦✨").length(), 6);
+	}
 }
